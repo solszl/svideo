9e472b4a54714d4573ba88e306311b2c18343451
振亮 孙 <solszl@163.com>
2018-11-04 23:00:57 +0800

    * modify svideo name to player

--- src/player.js
+++ src/player.js
@@ -4,10 +4,10 @@ import Exception from './error/Exception';
  * VIDEO播放器 函数类
  *
  * @export
- * @class SVideo
+ * @class Player
  * @author zhenliang.sun
  */
-export default class SVideo {
+class Player {
   constructor() {
     throw new Exception('No need to initilized, all method mount on player instance');
   }
@@ -16,3 +16,5 @@ export default class SVideo {
     // let player =
   }
 }
+
+export default Player;
