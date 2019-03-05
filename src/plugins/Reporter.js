341cf7a7dd1bed23589f5b51e80106c71b97f9fd
zhenliang.sun <solszl@163.com>
2019-03-05 15:06:56 +0800

    * 心跳和信息包里， 修改tt的规则， mp4 取消注释代码

--- src/plugins/Reporter.js
+++ src/plugins/Reporter.js
@@ -2,17 +2,6 @@ import qs from 'qs';
 import Model from '../core/Model';
 import Plugin from '../core/Plugin';
 
-const Code = {
-  /** 开始 */
-  Start: 92001,
-  /** 停止 */
-  End: 92002,
-  /** 心跳 */
-  HeartBeat: 92003,
-  /** 卡顿 */
-  Lag: 94001
-};
-
 const LIVE_CODE = {
   Start: 102001,
   Info: 102002,
@@ -48,13 +37,17 @@ export default class Reporter extends Plugin {
     this._infoPackInterval = 0;
     this._lagCount = 0;
     this.el = null; // 用来记录各种url的
-    this._tt = Date.now();
     this._bt = 0; // 卡顿时长
     this.basicInfo = {};
     this.enable = true;
     this._xhr = new XMLHttpRequest();
     this._infoPackCount = 0; // 24小时直播需要发 60*60*24/30
     this._lastDownloadSize = 0;
+
+    this._lastPlayTimeInfo = Date.now();
+    this._lastPlayTimeHeartbeat = Date.now();
+    this._playInfoDuration = 0;
+    this._playHeartbeatDuration = 0;
   }
 
   init(opts = {}) {
@@ -101,6 +94,8 @@ export default class Reporter extends Plugin {
     this._infoPack = 0;
     this._lastDownloadSize = 0;
     this._infoPackCount = 0;
+    this._lastPlayTimeInfo = 0;
+    this._lastPlayTimeHeartbeat = 0;
     this.el = null;
     if (this._xhr) {
       this._xhr.ontimeout = null;
@@ -127,6 +122,8 @@ export default class Reporter extends Plugin {
     }
     this.info('info', '启动日志上报');
     this.running = true;
+    this._lastPlayTimeInfo = Date.now();
+    this._lastPlayTimeHeartbeat = Date.now();
     clearInterval(this._infoPackInterval);
     this._infoPackInterval = setInterval(this._infoPack.bind(this), INFO_PACK_INTERVAL);
   }
@@ -169,6 +166,7 @@ export default class Reporter extends Plugin {
       xhr.send();
     } else {
       this.info('info', `日志上报配置文件[enable:false],不进行日志上报, code:${code}`);
+      this.info('info', `${JSON.stringify(obj)}`)
     }
 
     this.basicInfo.bt = 0;
@@ -200,7 +198,6 @@ export default class Reporter extends Plugin {
 
   _infoPack() {
     let obj = {
-      tt: Date.now() - this._tt,
       bc: this._lagCount,
       bt: this._bt
     };
@@ -208,6 +205,7 @@ export default class Reporter extends Plugin {
     // 信息报每隔30秒派发一个
     // 心跳包每分钟派发一个
     if (this._infoPackCount & 1) {
+      obj.tt = this._playHeartbeatDuration + Date.now() - this._lastPlayTimeHeartbeat;
       if (this.isLive) {
         this.fire(LIVE_CODE.HeartBeat, obj);
       } else {
@@ -224,37 +222,53 @@ export default class Reporter extends Plugin {
       obj.si = '';
       // 根据直播状态， 如果当前可播放， 那么上报2002 否则上报4001
       obj.errCode = this.player.readyState == 4 ? 2002 : 4001;
+      obj.tt = this._playInfoDuration + Date.now() - this._lastPlayTimeHeartbeat;
       this.fire(LIVE_CODE.Info, obj);
     } else {
+      obj.tt = this._playInfoDuration + Date.now() - this._lastPlayTimeHeartbeat;
       this.fire(VOD_CODE.Info, obj);
     }
 
     this._infoPackCount += 1;
+    this._playHeartbeatDuration = 0;
+    this._playInfoDuration = 0;
+    this._lastPlayTimeHeartbeat = Date.now();
+    this._lastPlayTimeInfo = Date.now();
   }
 
   __play(e) {
     if (!this.running) {
       this.start();
     }
-    this._tt = Date.now();
 
+    this._playInfoDuration += Date.now() - this._lastPlayTimeInfo;
+    this._playHeartbeatDuration += Date.now() - this._lastPlayTimeHeartbeat;
     if (!this.isLive) {
       this.fire(VOD_CODE.Resume, {});
     }
   }
 
   __pause(e) {
+    this._playInfoDuration += Date.now() - this._lastPlayTimeInfo;
+    this._playHeartbeatDuration += Date.now() - this._lastPlayTimeHeartbeat;
+    this._lastPlayTimeInfo = Date.now();
+    this._lastPlayTimeHeartbeat = Date.now();
+
     if (!this.isLive) {
       let obj = {
-        tt: Date.now() - this._tt,
         bc: this._lagCount
       };
       this.fire(VOD_CODE.Pause, obj);
     }
+
+    if (this.running) {
+      this.stop();
+    }
   }
 
   __waiting(e) {
-
+    this._lastPlayTimeInfo = Date.now();
+    this._lastPlayTimeHeartbeat = Date.now();
   }
 
   __ended(e) {
@@ -264,13 +278,16 @@ export default class Reporter extends Plugin {
 
   __lag(e) {
     this._lagCount += 1;
-
+    this._lastPlayTimeInfo = Date.now();
+    this._lastPlayTimeHeartbeat = Date.now();
     this.info('info', `接收到卡顿时间，开始计数，当前卡顿数量${this._lagCount}`);
   }
 
   __lagRecover(t) {
     this.info('info', `卡顿恢复了，消耗了${t}ms`);
     this._bt = t;
+    this._lastPlayTimeInfo = Date.now();
+    this._lastPlayTimeHeartbeat = Date.now();
   }
 
   __ready() {
