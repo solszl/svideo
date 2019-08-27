1913c22632d7d525040ffd254af04a22d7137966
zhenliang.sun <solszl@163.com>
2019-08-27 14:28:07 +0800

    + add error code file

---
+++ src/ErrorCode.js
@@ -0,0 +1,18 @@
+/**
+ *
+ * Created Date: 2019-08-27, 14:16:12 (zhenliang.sun)
+ * Last Modified: 2019-08-27, 14:24:41 (zhenliang.sun)
+ * Email: zhenliang.sun@gmail.com
+ *
+ * Distributed under the MIT license. See LICENSE file for details.
+ * Copyright (c) 2019 vhall
+ */
+
+// 1xxxxx   HLS 错误信息
+// 2xxxxx   FLV错误信息
+// 3xxxxx   原生错误信息
+// 4xxxxx   插件错误信息
+// 5xxxxx   内核错误信息
+
+/** 错误信息 */
+export const ErrorCode = {}
